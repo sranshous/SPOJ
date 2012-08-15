01ecfc2c46c4c304bd7c4c1a0b4103b67a2f5cd9
Stephen Ranshous <sranshous@gmail.com>
2012-08-14 23:07:35 -0400

    Working AC solution using memoization.

--- unsolved/TWENDS/TWENDS.java
+++ unsolved/TWENDS/TWENDS.java
@@ -22,6 +22,9 @@ class TWENDS {
     public static String[] input;
     public static int[][] memoization;
 
+    /*
+     * Main method
+     */
     public static void main(String[] args) {
         BufferedReader br = null;
 
@@ -44,34 +47,6 @@ class TWENDS {
                 System.out.println("In game " + gameNum++ + ", the greedy strategy might lose by as many as " + play(cards, 0, cards.length-1) + " points.");
                 input = br.readLine().split(" ");
             } while(Integer.parseInt(input[0]) != 0);
-
-            /*
-            do {
-                input = br.readLine().split(" ");
-                leftSide = 1;
-                rightSide = input.length - 1;
-                numCards = input.length - 1;
-
-                //p1 = new int[input.length-1];
-                //p2 = new int[input.length-1];
-                p1 = p2 = 0;
-
-                while(numCards > 0) {
-                    /*
-                     * Find difference between first pick and p2
-                     * then pick max between 2 avail.
-                     *
-                     * Find difference between other choice for first pick and p2
-                     * then pick max between 2 avail.
-                     *
-
-
-
-                    numCards -= 2;
-                }
-                gameNum++;
-            } while(Integer.parseInt(input[0]) != 0);
-            */
         }
         catch(Exception e) {
             e.getMessage();
@@ -79,10 +54,9 @@ class TWENDS {
         }
     }
 
-/*
- * Write recursively to try to figure it out then transform to DP
- */
-
+    /*
+     * Write recursively to try to figure it out then transform to DP
+     */
     public static int play(int[] cards, int left, int right) {
         if(left >= right) {
             return 0;
@@ -92,28 +66,21 @@ class TWENDS {
             return memoization[left][right];
 
         int l = 0, r = 0;
-        //int shared = play(cards, left+1, right-1);
 
         // you pick left
         if(cards[left+1] >= cards[right]) {
-            l = (cards[left] - cards[left+1]);
-            if(left+2 < cards.length)
-                l += play(cards, left+2, right);
+            l = cards[left] - cards[left+1] + play(cards, left+2, right);
         }
         else /*(cards[left+1] < cards[right])*/ {
             l =  (cards[left] - cards[right]) + play(cards, left+1, right-1);
-            //l = cards[left] - cards[right] + shared;
         }
 
         // you pick right
         if(cards[left] >= cards[right-1]) {
             r = (cards[right] - cards[left]) + play(cards, left+1, right-1);
-            //r = cards[right] - cards[left] + shared;
         }
         else /*(cards[left] < cards[right-1])*/ {
-            r = (cards[right] - cards[right-1]);
-            if(right-2 > 0)
-                r += play(cards, left, right-2);
+            r = cards[right] - cards[right-1] + play(cards, left, right-2);
         }
 
         int max = Math.max(l, r);
