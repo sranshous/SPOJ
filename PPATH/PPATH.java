4f69c2cd47f3fb91b248627a84d537185db9aca9
Stephen Ranshous <sranshous@gmail.com>
2012-10-01 19:39:00 -0400

    Need to implement some methods

--- PPATH/PPATH.java
+++ PPATH/PPATH.java
@@ -1,6 +1,7 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 
 /**
  * Approach: Use breadth first search
@@ -9,21 +10,99 @@ import java.io.InputStreamReader;
  * We continue this until we reach the target door number
  */
 
+/*
+ * TODO: Improve time by creating the entire graph (adjacency list) that
+ * will be used by the program the entire time. Never need to alter the graph
+ * only create new queues each time.
+ * TODO: Write the BFS using the adjacency list.
+ */
+
 public class PPATH {
-    // All primes for the problem are 4 digits
+    public static BufferedReader br = null;
+
+    /* All primes for the problem are 4 digits */
     public static final int MAX_NUM = 10000;
     public static boolean[] PRIMES = new boolean[MAX_NUM];
 
+    /* this will be used by BFS to make sure we dont waste time */
+    public static boolean[] visited;
+
+    /* Adjacency List */
+    public static ArrayList<ArrayList<Integer>> al;
+
+
     public static void main(String[] args) {
         /* Get primes */
-        for(int i = 0; i < PRIMES.length; i++)
+
+        int i, j;
+        for(i = 0; i < PRIMES.length; i++)
             PRIMES[i] = true;
 
-        for(int i = 2; i < PRIMES.length; i++)
+        for(i = 2; i < PRIMES.length; i++)
             if(PRIMES[i])
-                for(int j = 2*i; j < PRIMES.length; j += i)
+                for(j = 2*i; j < PRIMES.length; j += i)
                     PRIMES[j] = false;
 
-        //System.out.println(Arrays.toString(PRIMES));
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            int numCases = Integer.parseInt(br.readLine());
+
+            for(i = 0; i < numCases; i++) {
+                int roomNum = Integer.parseInt(br.readLine());
+                int targetNum = Integer.parseInt(br.readLine());
+                int cost;
+                Node root = new Node(roomNum, 0);
+
+                if((cost = BFS(root, targetNum)) == -1)
+                    System.out.println("Impossible");
+                else
+                    System.out.println(cost);
+            }
+        }
+        catch(Exception e) {
+            e.getMessage();
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * This method is what will find the cheapest path to
+     * the target prime.
+     * If found it will return the cost
+     * If not found it will return -1
+     */
+    public static int BFS(Node root, int target) {
+        visited = new boolean[MAX_NUM];
+        // Our queue for the breadth first search
+        ArrayDeque<Node> queue = new ArrayDeque<Node>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            
+        }
+
+        return -1;
+    }
+
+    /*
+     * Build the entire graph of primes for the program.
+     * It will be represented in an adjacency list.
+     */
+    public static void buildGraph() {
+        al = new ArrayList<ArrayList<Integer>>(MAX_NUM);
+
+        for(int i = 0; i < MAX_NUM; i++) {
+            al.add(new ArrayList<Integer>());
+        }
+    }
+}
+
+class Node {
+    int primeNum, cost;
+
+    public Node(int primeNum, int cost) {
+        this.primeNum = primeNum;
+        this.cost = cost;
     }
 }
